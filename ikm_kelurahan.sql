44af981c36c0ff310cbe6520a0348bcd3445cccb
afman42 <afifarman50@gmail.com>
2020-12-23 07:05:35 +0700

    perbaikan sql

--- ikm_kelurahan.sql
+++ ikm_kelurahan.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Localhost
+ Source Server         : localhot
  Source Server Type    : MySQL
  Source Server Version : 50724
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 05/12/2020 12:32:54
+ Date: 23/12/2020 07:05:09
 */
 
 SET NAMES utf8mb4;
@@ -29,18 +29,20 @@ CREATE TABLE `jawaban`  (
   `jawaban4` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `id_soal` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id_jawaban`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of jawaban
 -- ----------------------------
+INSERT INTO `jawaban` VALUES (1, 'sangat bagus', 'bagus', 'tidak bagus', 'sangat tidak bagus', 1);
+INSERT INTO `jawaban` VALUES (2, 'Bagus', 'Sangat Bagus', 'Tidak Bagus', 'Sangat Tidak Bagus', 2);
 
 -- ----------------------------
 -- Table structure for jawaban_user
 -- ----------------------------
 DROP TABLE IF EXISTS `jawaban_user`;
 CREATE TABLE `jawaban_user`  (
-  `id_jawaban_user` int(11) NOT NULL,
+  `id_jawaban_user` int(11) NOT NULL AUTO_INCREMENT,
   `jawaban1` int(11) NULL DEFAULT NULL,
   `jawaban2` int(11) NULL DEFAULT NULL,
   `jawaban3` int(11) NULL DEFAULT NULL,
@@ -49,16 +51,16 @@ CREATE TABLE `jawaban_user`  (
   `jawaban6` int(11) NULL DEFAULT NULL,
   `jawaban7` int(11) NULL DEFAULT NULL,
   `jawaban8` int(11) NULL DEFAULT NULL,
-  `jawaban9` int(11) NULL DEFAULT NULL,
-  `jawaban10` int(11) NULL DEFAULT NULL,
-  `id_responden` int(11) NULL DEFAULT NULL,
+  `id_responden` char(4) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `komentar` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
   PRIMARY KEY (`id_jawaban_user`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of jawaban_user
 -- ----------------------------
+INSERT INTO `jawaban_user` VALUES (3, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'R-01', 'mantap');
+INSERT INTO `jawaban_user` VALUES (4, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'R-02', 'Bagus');
 
 -- ----------------------------
 -- Table structure for kategori
@@ -70,33 +72,35 @@ CREATE TABLE `kategori`  (
   `persyaratan` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
   `status` int(1) NULL DEFAULT 0,
   PRIMARY KEY (`id_kategori`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of kategori
 -- ----------------------------
-INSERT INTO `kategori` VALUES (1, 'Mantap', '<p>Mantap</p>', 1);
+INSERT INTO `kategori` VALUES (1, 'Mantap', '<p>Mantap</p>', 0);
+INSERT INTO `kategori` VALUES (2, 'Survei Jalan', '<p>1. Penjelasan</p>', 1);
 
 -- ----------------------------
 -- Table structure for responden
 -- ----------------------------
 DROP TABLE IF EXISTS `responden`;
 CREATE TABLE `responden`  (
-  `id_responden` int(11) NOT NULL AUTO_INCREMENT,
+  `id_responden` char(4) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `tanggal` date NULL DEFAULT NULL,
-  `nama` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `nama` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `nip` varchar(25) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `umur` int(3) NULL DEFAULT NULL,
-  `jenis_kelamin` tinyint(1) NULL DEFAULT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `jenis_pendidikan` varchar(25) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `pekerjaan` varchar(25) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `id_kategori` int(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_responden`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+  `id_kategori` int(11) NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of responden
 -- ----------------------------
+INSERT INTO `responden` VALUES ('R-01', '2020-12-21', 'Armandos', '12345678', 21, 'Laki-laki', 'SMA Kebawah', 'PNS', 1);
+INSERT INTO `responden` VALUES ('R-02', '2020-12-22', 'Arman', '1912314', 22, 'Laki-laki', 'SMA Kebawah', 'PNS', 1);
 
 -- ----------------------------
 -- Table structure for soal
@@ -107,11 +111,13 @@ CREATE TABLE `soal`  (
   `soal` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
   `id_kategori` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id_soal`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of soal
 -- ----------------------------
+INSERT INTO `soal` VALUES (1, 'abc', 1);
+INSERT INTO `soal` VALUES (2, 'Surat Izin Tanah', 1);
 
 -- ----------------------------
 -- Table structure for users
@@ -119,15 +125,18 @@ CREATE TABLE `soal`  (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `username` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `password` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `password` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `foto` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `level` enum('admin','kepala_lurah') CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id_user`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (1, 'admin', 'admin', 'admin');
+INSERT INTO `users` VALUES (1, 'admin', 'admin', 'admin', 'uploads/Screenshot_5.png', 'admin');
+INSERT INTO `users` VALUES (2, 'lurah', 'lurah', '123456', NULL, 'kepala_lurah');
 
 SET FOREIGN_KEY_CHECKS = 1;
